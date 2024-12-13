bb78afbbb69a2efe3ae8ff27c249b151ad2086af
Codeuzz <belinda@louise.life>
2024-12-13 10:52:45 +0100

    fix routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
       <h1 className="text-7xl font-extrabold italic">Poke Shop</h1>
       <h3 className="text-4xl font-semibold italic">Buy the best Pokemon!</h3>
       <Link
-        to="/pokemon-list"
+        to="/PokeShop/pokemon-list"
         className="text-lg border-2 border-black bg-amber-400 shadow-black shadow-md py-2 px-6 rounded-3xl hover:bg-amber-500"
       >
         Get My Pokemon
       </Link>
-      <img src="./public/firstpoke.png" className="max-w-xl" /> 
+      <img src="./firstpoke.png" className="max-w-xl" /> 
     </div>
   );
 };
