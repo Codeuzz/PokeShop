ffb97ef42f5619c44a8109c932715e5484c8789c
Codeuzz <belinda@louise.life>
2024-12-13 11:05:49 +0100

    add AddToCart message

--- src/components/shared/AddToCart.jsx
+++ src/components/shared/AddToCart.jsx
@@ -1,16 +1,20 @@
 import { useDispatch } from "react-redux";
 import { addCartItem } from "../../utils/slices/cartSlice";
+import { useState } from "react";
 
 const AddToCart = ({item}) => {
     const dispatch = useDispatch();
+    const [message, setMessage] = useState('');
   
     const addItem = (pokemon) => {
       dispatch(addCartItem(pokemon));
-  
-      
+
+      setMessage(`${pokemon.name} added to cart!`);
+      setTimeout(() => setMessage(""), 2000);      
     };
 
     return (
+        <>
         <button
             className="bg-purple-300 shadow-md shadow-black py-1 px-3 rounded-full absolute -right-2 -top-2 hover:bg-amber-400"
             onClick={() => addItem(item)}
@@ -18,6 +22,8 @@ const AddToCart = ({item}) => {
           >
             <i className="fa-solid fa-plus" />
         </button>
+        {message && <p className="bg-green-200 py-1 px-2 rounded-md font-bold text-purple-500 absolute -top-6">{message}</p>}
+        </>
     )
 }
 
